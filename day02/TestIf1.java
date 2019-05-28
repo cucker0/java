822c63dbe0bfe094d70bd0baa952bc7f5e340add
cucker <hanxiao2100@qq.com>
2019-05-28 14:57:44 +0800

    if - else 分支

---
+++ day02/TestIf1.java
@@ -0,0 +1,27 @@
+/*
+if - else分支语句
+
+执行：javac -encoding utf8 TestIf1.java && java TestIf1
+* */
+
+class TestIf1 {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("你的年龄：" + age + "岁");
+        if (age > 18) {
+            System.out.println("你已经成年");
+        } else {
+            System.out.println("小屁孩一个... 该干吗干吗去");
+        }
+
+        if (age > 120 || age < 0) {
+            System.out.println("成仙了");
+        } else if (age < 30) {
+            System.out.println("还没到儿立之年");
+        } else {
+            System.out.println("已过不惑之年");
+        }
+
+
+    }
+}
