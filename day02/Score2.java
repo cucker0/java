f667502c8863d50ecf7608ee34bf94f06c371593
cucker <hanxiao2100@qq.com>
2019-05-29 22:20:45 +0800

    add switch判断成绩等级

---
+++ day02/Score2.java
@@ -0,0 +1,44 @@
+/*
+
+switch编写程序：从键盘上读入一个学生成绩，存放在变量score中，根据score的值输出其对应的成绩等级：
+
+score>=90        等级：A
+70=<score<90     等级: B
+60=<score<70     等级: C
+score<60         等级：D
+
+
+* */
+
+import java.util.Scanner;
+
+class Score2 {
+    public static void main(String[] args) {
+        System.out.println("Input score：");
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+
+        if (score < 0 || score > 100) {
+            System.out.println("Score input erro!!");
+        } else {
+            score /= 10;
+            char level = 'D';
+            switch (score) {
+                case 10:
+                case 9:
+                    level = 'A';
+                    break;
+                case 8:
+                case 7:
+                    level = 'B';
+                    break;
+                case 6:
+                    level = 'C';
+                    break;
+                default:
+                    level = 'D';
+            }
+            System.out.println(level);
+        }
+    }
+}
