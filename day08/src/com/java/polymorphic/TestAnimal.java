2376301aacd127a9861a0f9dedce9bee380da1c0
cucker <hanxiao2100@qq.com>
2019-06-13 15:16:32 +0800

    类的多态应用

--- day08/src/com/java/polymorphic/TestAnimal.java
+++ day08/src/com/java/polymorphic/TestAnimal.java
@@ -6,11 +6,50 @@
 
 package com.java.polymorphic;
 
+import java.util.concurrent.TimeoutException;
+
 public class TestAnimal {
     public static void main(String[] args) {
+        Animal an1 = new Dog();
+        an1.eat();
+        an1.jump();
+//        an1.bark(); // 不能直接调用
+
+        System.out.println();
+        Animal an2 = new Cat("卢布朵");
+        an2.eat();
+        an2.jump();
+
+        System.out.println();
+        TestAnimal t = new TestAnimal();
+        t.call(an1);
+        t.call(an2);
+        t.hobby(an1);
+        t.hobby(an2);
+
+
 
     }
 
+    void call(Animal animal) {
+        if (animal instanceof Dog) {
+            Dog an = (Dog)animal;
+            an.call();
+        } else if (animal instanceof Cat) {
+            Cat an = (Cat)animal;
+            an.call();
+        }
+    }
+
+    public void hobby(Animal animal) {
+        if (animal instanceof Dog) {
+            Dog dd = (Dog)animal;
+            dd.call();
+        } else if (animal instanceof Cat) {
+            Cat cc = (Cat)animal;
+            cc.fishFarming();
+        }
+    }
 }
 
 class Animal {
@@ -21,6 +60,10 @@ class Animal {
 
     }
 
+    public Animal(String name) {
+        setName(name);
+    }
+
     // method
     public void eat() {
         System.out.println("进食");
@@ -28,6 +71,14 @@ class Animal {
 
     public void jump() {
         System.out.println("跳");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
 
@@ -42,6 +93,39 @@ class Dog extends Animal {
         System.out.println("狗急跳墙");
     }
 
+    public void call() {
+        // 狗吠叫
+        System.out.println("汪汪汪...");
+    }
+}
 
+class Cat extends Animal {
+
+    // constructor
+    public Cat() {
+        super();
+    }
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    // method
+    public void eat() {
+        System.out.println("猫吃鱼");
+    }
+
+    public void jump() {
+        System.out.println("猫跳上墙");
+    }
+
+    public void fishFarming() {
+        // 种鱼
+        System.out.println("猫种鱼");
+    }
+
+    public void call() {
+        System.out.println("猫喵喵叫...");
+    }
 
 }
