0b564c0e793fe5b8f10fb8472710c9a747fb9cb6
cucker <hanxiao2100@qq.com>
2019-07-02 16:02:49 +0800

    Hashtable子类：Properties

---
+++ day13/src/com/java/www/PropertiesTest.java
@@ -0,0 +1,36 @@
+/*
+Hashtable 子类: Properties
+
+* */
+
+package com.java.www;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesTest {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("jdbc.properties"));
+            String host = properties.getProperty("host");
+            String dbname = properties.getProperty("dbname");
+            String user = properties.getProperty("user");
+            String password = properties.getProperty("password");
+
+            System.out.println("host: " + host);
+            System.out.println("dbname: " + dbname);
+            System.out.println("user: " + user);
+            System.out.println("password: " + password);
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
