c7d1ebe33a4b4b5a10ea4157851976993d759648
cucker <hanxiao2100@qq.com>
2019-07-04 23:32:23 +0800

    DAO

---
+++ day14/src/com/java/www/DAO.java
@@ -0,0 +1,26 @@
+/*
+模拟DAO，database access object
+
+* */
+
+package com.java.www;
+
+import java.util.List;
+
+public class DAO<T> {
+    public void add(T t) {
+
+    }
+
+    public T get(int index) {
+        return null;
+    }
+
+    public List<T> getList(int index) {
+        return null;
+    }
+
+    public void delete(int index) {
+
+    }
+}
