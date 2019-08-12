5c80d56eda09edfe89ddd141c083c54f71775836
cucker <hanxiao2100@qq.com>
2019-08-12 18:30:34 +0800

    add Customer类

---
+++ project02/src/com/java/com/Customer.java
@@ -0,0 +1,19 @@
+/*
+客户
+* */
+
+package com.java.com;
+
+public class Customer {
+    // 实例属性
+    private String name;
+    private boolean sex; // 性别 0: 女性， 1: 男性
+    private int age;
+    private String phone;
+    private String email;
+
+    // 构造器
+
+    // 方法
+    
+}
