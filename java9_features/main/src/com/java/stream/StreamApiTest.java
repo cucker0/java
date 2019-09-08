2430a2fb30a155b41245bffd72468f13291e74a5
cucker <hanxiao2100@qq.com>
2019-09-08 16:58:23 +0800

    update java 9

---
+++ java9_features/main/src/com/java/stream/StreamApiTest.java
@@ -0,0 +1,4 @@
+package com.java.stream;
+
+public class StreamApiTest {
+}
--- java9_features/main/src/com/java/www/CollectionMapTest.java
+++ java9_features/main/src/com/java/www/CollectionMapTest.java
@@ -2,9 +2,7 @@ package com.java.www;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class CollectionMapTest  {
     /**
@@ -27,6 +25,48 @@ public class CollectionMapTest  {
         List<String> list2 = Collections.unmodifiableList(list);
         list2.forEach(System.out::println);
 //        list2.add("lisa"); // 报异常 java.lang.UnsupportedOperationException
+
+    }
+
+    @Test
+    public void test2() {
+        // 同样 创建一个只读集合。把一个可读写的集合转成只读集合
+        List<Integer> list = Collections.unmodifiableList(Arrays.asList(22, 111, 33));
+        Set<Integer> set = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(11, 22, 33)));
+        Map<String, Integer> map = Collections.unmodifiableMap(new HashMap<>(){
+            {
+                put("kk", 66);
+                put("qq", 77);
+                put("mm", 99);
+            }
+        });
+        list.add(99);
+        set.add(44);
+        map.put("jj", 100);
+    }
+
+    /**
+     * java 9中创建只读集合
+     * Xxx.of()
+     * Map.ofEntries()
+     */
+    @Test
+    public void test3() {
+        List<Integer> list = List.of(11, 33, 55);
+        list.forEach(System.out::println);
+//        list.add(66); // 报java.lang.UnsupportedOperationException 异常
+
+        Set<Integer> set = Set.of(33, 22, 55, 66);
+//        set.add(66); // 不可修改
+
+        // 创建只读Map方式1
+        Map<String, Integer> map = Map.of("k1", 33, "k2", 55, "k3", 99);
+        map.forEach((k, v) -> System.out.println(k + ": "+ v));
+        map.put("k5", 77); // 不可修改
+
+        // 创建只读Map方式2
+        Map<String, Integer> map1 = Map.ofEntries(Map.entry("k1", 33), Map.entry("k2", 66));
+        map1.put("k3", 88); // 不可修改
     }
 
 }
