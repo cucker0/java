e3d0388596987c84760554d904d35f2baf6029a8
cucker <hanxiao2100@qq.com>
2019-09-19 00:35:05 +0800

    update NioTcpNonblockingTest

--- NIO/src/com/java/tcp/NioTcpNonblockingTest.java
+++ NIO/src/com/java/tcp/NioTcpNonblockingTest.java
@@ -5,10 +5,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.time.LocalDateTime;
 import java.util.Iterator;
 
@@ -25,29 +22,68 @@ import java.util.Iterator;
 public class NioTcpNonblockingTest {
     @Test
     public void server() {
-        // 1. 获取服务端通道
-        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        ServerSocketChannel serverSocketChannel = null;
+        try {
+            // 1. 获取服务端通道
+            serverSocketChannel = ServerSocketChannel.open();
 
-        // 2. 配置服务端通道为非阻塞模式
-        serverSocketChannel.configureBlocking(false); // 默认是阻塞模式
+            // 2. 配置服务端通道为非阻塞模式
+            serverSocketChannel.configureBlocking(false); // 默认是阻塞模式
 
-        // 3. 绑定InetSocketAddress
-        serverSocketChannel.bind(new InetSocketAddress(8080));
+            // 3. 绑定InetSocketAddress
+            serverSocketChannel.bind(new InetSocketAddress(8080));
 
-        // 4. 创建一个选择器
-        Selector selector = Selector.open();
+            // 4. 创建一个选择器
+            Selector selector = Selector.open();
 
-        // 5. 把serverSocketChannel注册到选择器，并指定监听ACCEPT的事件
-        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            // 5. 把serverSocketChannel注册到选择器，并指定监听ACCEPT的事件
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
-        // 6. 轮询获取选择器上 已经准备就绪的事件
-        while (selector.select() > 0) { // selector.select(): 表示就绪的事件数量
-            // 7. 获取当前选择器中所有注册的 就绪监听事件(选择键)
-            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            // 6. 轮询获取选择器上 已经准备就绪的事件
+            while (selector.select() > 0) { // selector.select(): 表示就绪的事件数量
+                // 7. 获取当前选择器中所有注册的 就绪监听事件(选择键)
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    // 8. 获取已准备就绪的事件
+                    SelectionKey key = iterator.next();
+                    // 9. 判断事件类型
+                    if (key.isAcceptable()) {
+                        // 10. 接收事件就绪了，获取与客户端的通道
+                        SocketChannel socketChannel = serverSocketChannel.accept();
+                        // 11. 把与客户端连接的通道配置为非阻塞的
+                        socketChannel.configureBlocking(false);
+                        // 12. 将此通道的下一个事件read注册到选择器上
+                        socketChannel.register(selector, SelectionKey.OP_READ);
+                    } else if (key.isReadable()) {
+                        // 13. 获取 读就绪 的通道
+                        SocketChannel readChannel = (SocketChannel) key.channel();
 
+                        // 14. 读取数据
+                        ByteBuffer buffer = ByteBuffer.allocate(1024 * 8);
+                        int len;
+                        while ((len = readChannel.read(buffer)) != -1) {
+                            buffer.flip();
+                            System.out.println(new String(buffer.array()));
+                            buffer.clear();
+                        }
+                    }
 
+                    // 15. 取消选择键SelectionKey，每个SelectionKey只能执行一次
+                    iterator.remove();
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (serverSocketChannel != null) {
+                try {
+                    serverSocketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
 
     }
 
