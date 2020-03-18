d84352b30d74cf9db0cdecac1a3fa009a97c44ab
cucker <hanxiao2100@qq.com>
2020-03-18 11:50:10 +0800

    update Set \ Map 利用匿名类的静态代码块赋初值

--- day13/src/com/java/www/ListSetMap_SpecifyInitialValuesAtNewBuildTime.java
+++ day13/src/com/java/www/ListSetMap_SpecifyInitialValuesAtNewBuildTime.java
@@ -1,7 +1,7 @@
 /*
 * List、Set、Map在创建对象时指定初始值
 * 格式: new List/Set/Map实现类() {{}};
- * 外面的这个 {} 为匿名类，里面的{}为代码块。
+ * 外面的这个 {} 为匿名类，里面的{}为静态代码块。
  * 这种方式只有jdk >= 9 才支持
  * 
 * 注意： 慎用， 非静态内部类/ 匿名内部类包含了外围实例的引用， 如果拥有比外部类更长的生命周期，有内存泄露隐患。
@@ -21,7 +21,8 @@ public class ListSetMap_SpecifyInitialValuesAtNewBuildTime {
     @Test
     public void test1() {
         List<String> list = new ArrayList<>() { // 创建一个继承于ArrayList 匿名子类对象
-            {   // 代码块
+            // 静态代码块，在加载类的时候就执行
+            {
                 add("我的楼兰--云朵"); // this.add("我的楼兰--云朵");
                 add("你还是从前的你吗--天籁天");
                 add("我是你的格桑花--欣宝儿");
@@ -49,13 +50,11 @@ public class ListSetMap_SpecifyInitialValuesAtNewBuildTime {
      */
     @Test
     public void test3() {
-        Map<String, Boolean> map1 = new TreeMap<>() {
-            {
-                put("北京", true);
-                put("天津", false);
-                put("深圳", true);
-            }
-        };
+        Map<String, Boolean> map1 = new TreeMap<>() {{
+            put("北京", true);
+            put("天津", false);
+            put("深圳", true);
+        }};
 
         System.out.println(map1);
     }
